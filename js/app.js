217ff4882bf14b2d609ae69913699490241ec5b1
OTÁVIO PINHEIRO GARCIA <otavio.garcia5@senaisp.edu.br>
2026-09-02 21:15:36 -0300

    feat: alterações em html, css e js

---
+++ js/app.js
@@ -0,0 +1,4 @@
+// Monitoramento básico para garantir a inicialização correta do sistema
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Aplicação WebAR carregada com sucesso!");
+});
